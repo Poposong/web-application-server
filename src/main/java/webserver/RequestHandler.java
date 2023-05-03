e8f41e47661373d77238ef9c658e3e91c4a849df
정예진 <psi1908@naver.com>
2023-05-03 11:03:53 +0900

    2nd - commit

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,11 +9,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import db.DataBase;
 import model.User;
 import util.HttpRequestUtils;
 import util.IOUtils;
@@ -45,96 +48,125 @@ public class RequestHandler extends Thread {
 			if (line == null) {
 				return;
 			}
-			
-			System.out.println(line); // header 출력
-			
-			// 2단계
-			String[] lines = line.split(" ");
 
-			String url = lines[1];
- 
+			// 2단계
+			String[] lines = line.split(" "); // Http header를 나눈다.
+
+			String url = lines[1]; // 요청 url
+
 			String[] root = url.split("/");
 			
-			
-			if (root[1].equals("index.html")) { // TODO : GET /index.html
-				DataOutputStream dos = new DataOutputStream(out);
-				// 3단계
-				byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath()); // index.html의 태그가 body[]에
-																						// 담긴다.
-				response200Header(dos, body.length);
-				responseBody(dos, body);
-			} else if (root[1].equals("user")) { // TODO : GET /user/create?userId=yejin990508&password=1234&name=yejin
-				// .split("?")[1]
-				 
-				if(root[2].equals("form.html")) { // TODO : POST /user/form.html
-					System.out.println("come!!!!!");
-				}else if(root[2].split("\\?")[0].equals("create")) {
-					String path = root[2].split("\\?")[1];
-					System.out.println("path : "+ path);
-					Map<String, String> map = HttpRequestUtils.parseQueryString(path);
+			// Http Header를 분석한다.
+			int contentLength = 0; 
+			boolean logined = false;
+			while (!"".equals(line)) {
 
-					User user = new User(map.get("userId"), map.get("password"), map.get("name"), "email");
-					
-					System.out.println(user.toString());
+				line = br.readLine();
+
+				if (line.contains("Content-Length")) {
+					contentLength = Integer.parseInt(line.split(": ")[1]);
+				} else if (line.contains("Cookie")) {
+					logined = isLogin(line);
 				}
-				
+
 			}
 			
 			
+
+			if (url.equals("/user/create")) { // 회원가입
+				String pathVariable = IOUtils.readData(br, contentLength);
+
+				Map<String, String> params = HttpRequestUtils.parseQueryString(pathVariable);
+				
+				User user = new User(params.get("userId"), params.get("password"), params.get("name"),
+						params.get("email"));
+
+				DataBase.addUser(user);
+
+				DataOutputStream dos = new DataOutputStream(out);
+
+				response302Header(dos, "/index.html");
+
+			} else if (url.equals("/user/login")) { // 로그인
+				String pathVariable = IOUtils.readData(br, contentLength);
+
+				Map<String, String> params = HttpRequestUtils.parseQueryString(pathVariable);
+				
+				User user = DataBase.findUserById(params.get("userId"));
+
+				if (user == null) {// DB에 회원의 정보가 없는 경우
+					responseResource(out, "/user/login_failed.html");
+					return;
+				}
+
+				if (user.getPassword().equals(params.get("password"))) { // DB에 저장한 비밀번호와 사용자가 입력한 비밀번호가 동일한 경우
+					DataOutputStream dos = new DataOutputStream(out);
+					response302LoginSuccessHeader(dos); // Header 요청 라인의 상태 코드를 302로 변경하고 Location을 추가한다.
+				} else {
+					responseResource(out, "/user/login_failed.html"); // 로그인에 실패한 경우
+				}
+			}else if(url.equals("/user/list")) { // 사용자의 목록을 조회한다.
+				if(!logined) {
+					responseResource(out, "/user/login.html");
+					return;
+				}
+				Collection<User> userList = DataBase.findAll();
+				StringBuilder sb = new StringBuilder();
+				sb.append("<table border='1'>");
+				sb.append("<tr> <td>아이디</td> <td>이름</td> <td>이메일</td> </tr>");
+				for(User user : userList) {
+					sb.append("<tr>");
+					sb.append("<td>"+user.getUserId()+"</td>");
+					sb.append("<td>"+user.getName()+"</td>");
+					sb.append("<td>"+user.getEmail()+"</td>");
+					sb.append("</tr>");
+				}
+				sb.append("</table>");
+				byte[] body = sb.toString().getBytes();
+				DataOutputStream dos = new DataOutputStream(out);
+				response200Header(dos, body.length);
+				responseBody(dos, body);
+			}else if(url.endsWith(".css")) {
+				DataOutputStream dos = new DataOutputStream(out);
+				byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+				response200CssHeader(dos, body.length);
+				responseBody(dos, body);
+			}else {
+				responseResource(out, url);
+			}
+			
+
+//			if (lines[0].equals("POST")) { // POST 방식으로 요청이 들어오는 경우
 //
-//			if (lines[0].equals("GET")) {
-//
-//				if (root.equals("index.html")) { // TODO : GET /index.html
-//					DataOutputStream dos = new DataOutputStream(out);
-//					// 3단계
-//					byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath()); // index.html의 태그가 body[]에
-//																							// 담긴다.
-//					response200Header(dos, body.length);
-//					responseBody(dos, body);
-//				} else if (root.equals("user")) { // TODO : GET user/create?userId=yejin990508&password=1234&name=yejin
-//					// .split("?")[1]
-//					String path = url.split("/")[2].split("\\?")[1];
-//					Map<String, String> map = HttpRequestUtils.parseQueryString(path);
-//
-//					User user = new User(map.get("userId"), map.get("password"), map.get("name"), "email");
-//					
-//					System.out.println(user.toString());
-//
-//				}
-//			} else if (lines[0].equals("POST")) {
-//				if (root.equals("user")) { // TODO : POST /user/create
-//					String move = url.split("/")[2]; // create
-//					
-//					int ContentLength = 0; // Content-Length
-//					String path = ""; // userId=javaJig&password=password&name=JaeSung
-//					
-//					
-//					while(!"".equals(line)) {
-//						System.out.println(line);
-//						
-//						if(line.contains("?")) {
-//							path = line.split("\\?")[1].split(" ")[0];
-//							System.out.println("path : "+ path);
-//						}else if(line.contains(":")) {
-//							String[] str = line.split(": ");
-//							if(str[0].equals("Content-Length")) {
-//								ContentLength = Integer.parseInt(str[1]);
-//							}
-//						}
-//						
-//						
-//						line = br.readLine();
-//
-//					}
-//		
-//				}
+//				
+//			} else if (lines[0].equals("GET")) { // GET 방식으로 요청이 들어오는 경우
+//				responseResource(out, url);
 //			}
 
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
 	}
-
+	
+	// ● isLogin : Cooki의 상태를 반환하는 메소드 
+	//             Ex) line의 형태 → Cookie: logined=true
+	public boolean isLogin(String line) {
+		String state = line.split(": ")[1].split("=")[1];
+		return Boolean.parseBoolean(state);
+	}
+	
+	// ● response302Header : Header의 상태 코드를 302으로 하고 url의 경로로 redirect 하는 메소드
+	private void response302Header(DataOutputStream dos, String url) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+			dos.writeBytes("Location: " + url + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+	
+	// ● response200Header : Header의 상태 코드를 200으로 하고 pathVariable의 길이를 반환하는 메소드
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
 		try {
 			dos.writeBytes("HTTP/1.1 200 OK \r\n");
@@ -145,13 +177,44 @@ public class RequestHandler extends Thread {
 			log.error(e.getMessage());
 		}
 	}
-
+	
+	
 	private void responseBody(DataOutputStream dos, byte[] body) {
 		try {
 			dos.write(body, 0, body.length);
 			dos.flush();
 		} catch (IOException e) {
 			log.error(e.getMessage());
+		}
+	}
+
+	private void responseResource(OutputStream out, String url) throws IOException {
+		DataOutputStream dos = new DataOutputStream(out);
+		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+		response200Header(dos, body.length);
+		responseBody(dos, body);
+	}
+	
+	// ● response302LoginSuccessHeader : 로그인을 했으므로 Cookie의 상태를 true로 만들고 index.html로 redirect하는 메소드
+	private void response302LoginSuccessHeader(DataOutputStream dos) {
+
+		try {
+			dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+			dos.writeBytes("Set-Cookie: logined=true \r\n");
+			dos.writeBytes("Location: /index.html \r\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void response200CssHeader(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 200 OK \r\n");
+			dos.writeBytes("Content-Type: text/css\r\n");
+			dos.writeBytes("Content-Length: "+lengthOfBodyContent+"\r\n");
+			dos.writeBytes("\r\n");
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
